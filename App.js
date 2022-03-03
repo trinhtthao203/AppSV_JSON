7ca4bdefe2b75d3553c7b7b28e091fb3e6898cbc
trinhtthao203 <trinhtthao203@gmail.com>
2022-03-03 14:43:40 +0700

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {createAppContainer} from 'react-navigation';
+import AppNavigator from './AppNavigator';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import axios from 'axios';
+
+const AppContainer = createAppContainer(AppNavigator);
+
+//ngrok
+axios.defaults.baseURL='http://9a90dae94684.ngrok.io';
+export default class App extends React.Component {
+  render(){    
+    return (
+      <AppContainer/>
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screen/FromHOC.js
@@ -0,0 +1,108 @@
+
+import React from 'react';
+import { StyleSheet, Alert} from 'react-native';
+import { Container, Content, Form, Item, Input, Label ,Button} from 'native-base';
+import axios from 'axios';
+import ActivityScreen from './ActivityScreen';
+import Mybutton from '../components/MyButton';
+class FromHOC extends React.Component {
+  static navigationOptions = {
+    title: 'Học Chi Tiết',
+  };  
+
+  state={isLoading:true};
+
+  componentDidMount(){
+    const {navigation} = this.props;
+    const id= navigation.getParam('idHOC'); 
+    const mssv= navigation.getParam('msSV');  
+    const mamon= navigation.getParam('maMH');   
+    const diem= navigation.getParam('diemHOC');   
+    this.get_data(id,mssv,mamon,diem);
+  }  
+
+  get_data(id,mssv,mamon,diem){
+    this.setState({
+      id:id,
+      mssv:mssv,
+      mamon:mamon,
+      diem:diem.toString()
+    })
+  }
+
+   handleSave(){
+    const {id,mssv,mamon,diem}=this.state;
+    if(diem==""){
+      Alert.alert("WARNING!!!","Lỗi nhập liệu vui lòng kiểm tra các dữ liệu bắt buộc đã được điền");
+    }else{
+      this.update(id,mssv,mamon,diem);
+      this.props.navigation.state.params.refresh();
+    }
+    
+    this.props.navigation.navigate("Hoc");
+  }
+
+  update(id,mssv,mamon,diem){
+    axios({
+            method:'put',
+            url:`/hoc/${id}`,
+            data:{
+              mssv:mssv,
+              mamon:mamon,
+              diem:diem
+            }
+          })
+          .then(res=>{
+            res.data;
+            this.setState({isLoading:false})
+            console.log(res);
+            Alert.alert("INFO","Học đã được sửa");
+          })
+          .catch(err=>{
+            console.log(err);
+          })
+}
+
+      render() {
+       if(this.state.isLoading){
+        return<ActivityScreen/>
+      }
+       
+      return (
+         <Container>
+          <Content>
+            <Form>
+              <Item stackedLabel>
+                <Label>MSSV*</Label>
+                <Input editable={false} onChangeText={(val)=>this.setState({mssv:val})} value={this.state.mssv} />
+              </Item>
+              <Item stackedLabel>
+                <Label>Tên môn học*</Label>
+                <Input editable={false} onChangeText={(val)=>this.setState({hten:val})} value={this.state.mamon}/>
+              </Item>
+              <Item stackedLabel>
+                <Label>Điểm *</Label>
+                <Input 
+                  keyboardType = 'numeric'
+                  onChangeText={(val)=>this.setState({diem:parseFloat(val)})}
+                  value={this.state.diem}
+                /> 
+              </Item>              
+              <Mybutton title="Cập nhật chỉnh sửa" onClick={()=>this.handleSave()} />
+              <Label style={{color:'red'}}>*Bắt buộc</Label>
+            </Form>
+          </Content>
+        </Container>
+      );
+    }
+  }
+
+  export default FromHOC;
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    }
+  });
